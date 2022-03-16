f2de4ef72157fb9a6d081ddf9e9712c46654b737
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-16 22:52:53 +0200

    exercie 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS"
+    }
+    let myArray = [1, 1];
+    for (let i = 2; i < num; i++) {
+        myArray[i] = myArray[i-1] + myArray[i-2];
+    }
+    return myArray[myArray.length-1]
 };
 
 // Do not edit below this line
