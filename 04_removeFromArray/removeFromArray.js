0fd3985864ad55612f0c31d4cdab62d685d6bb99
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-16 16:14:21 +0200

    4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...args) {
+    args.forEach(element=>{
+        for (let i = 0; i < myArray.length; i++) {
+            if (element === myArray[i]){
+                myArray.splice(i, 1)
+                console.log(myArray);
+            }
+        }
+    })
+    console.log(myArray)
+    return myArray
 };
 
 // Do not edit below this line
