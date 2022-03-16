10c763c543a4e2e842d17dac5ab188e8fe92117d
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-16 22:34:14 +0200

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    string = string.split(" ").join("").toLowerCase().replace(/[^a-zA-Z]/g, '')
+    let first_letter_index = 0;
+    let last_letter_index = string.length - 1;
+    for (let i = 0; i < string.length/2; i++) {
+        a = string[first_letter_index]
+        b = string[last_letter_index]
+        if (string[first_letter_index] === string[last_letter_index]){
+            first_letter_index += 1;
+            last_letter_index -=1 ;
+        }else {
+            return false
+        }        
+    }
+    return true
 };
 
 // Do not edit below this line
