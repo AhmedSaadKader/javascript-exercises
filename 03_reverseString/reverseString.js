9ad7628e121db987fa142bbf3e166581ef24a0fd
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-16 13:22:49 +0200

    solved third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string = string.split("").reverse().join("")
+    // console.log(string)
+    return string
 };
 
 // Do not edit below this line
